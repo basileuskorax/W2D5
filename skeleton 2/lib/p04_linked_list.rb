df615065743617c6400857f81e8b138fb006de31
App Academy Student <kjjafshf>
2018-01-26 14:44:57 -0500

    Complete linked list class

--- skeleton 2/lib/p04_linked_list.rb
+++ skeleton 2/lib/p04_linked_list.rb
@@ -19,7 +19,12 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new()
+    @tail = Node.new()
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +33,60 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each{ |node| return node.val if node.key == key }
+    nil
   end
 
   def include?(key)
+    each{ |node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.prev = last
+    last.next = new_node
+    @tail.prev = new_node
+    new_node.next = @tail
   end
 
   def update(key, val)
+    return nil if empty?
+    each{ |node| node.val = val if node.key == key}
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.next.prev = node.prev
+        node.prev.next = node.next
+      end
+    end
   end
 
   def each
+    yield(first)
+    next_node = first.next
+
+    until next_node == @tail
+      yield(next_node)
+      next_node = next_node.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
